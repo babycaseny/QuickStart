147244eef70d6a8c1a312f29f06d101cd2d2bdee
Tom Chiu @ nxTomo <tom.chiu@nxtomo.hk>
2015-02-23 18:39:28 +0800

    Adding shellscripts from Ubuntu machine

---
+++ ConnectToTomChiuHome.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+ssh -i /media/windows/Users/tom.chiu/Dropbox/nxTomo/TomChiuHome.pem ubuntu@54.186.119.33
---
+++ ConnectToWebogram.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+ssh -i /media/windows/Users/tom.chiu/Dropbox/nxTomo/WebogramTest.pem ubuntu@54.191.148.41
