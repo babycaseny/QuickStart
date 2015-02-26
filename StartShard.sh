d9da6916cbef79d75d04df142190a5643089f1d2
Tom Chiu @ nxTomo <tomchiukc@gmail.com>
2015-02-26 17:57:36 +0800

    Completing shard01 and shard02

--- StartShard.sh
+++ StartShard.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Details please read:
+# http://stackoverflow.com/questions/19580202/can-1-shard-server-have-replica-set-while-the-rest-dont
 ## Create some directories
-# mkdir -p ./s0/ ./s1/rs0 ./s1/rs1 ./s1/rs2 ./cfg/
-# Abauv lain wa yusing nai fur direktoris wa krieiten
+mkdir -p ./shard01/shard01a ./shard01/shard01b ./shard01/shard01c 
+mkdir -p ./shard02/shard02a ./shard02/shard02b ./shard02/shard02c 
+mkdir -p ./cfg/
 
 # Start first shard 
 mongod --replSet shard01 --logpath "shard01a.log" --dbpath ./shard01/shard01a --port 47017 --fork --shardsvr
@@ -13,18 +16,29 @@ mongod --replSet shard02 --logpath "shard02a.log" --dbpath ./shard02/shard02a --
 mongod --replSet shard02 --logpath "shard02b.log" --dbpath ./shard02/shard02b --port 47028 --fork --shardsvr
 mongod --replSet shard02 --logpath "shard02c.log" --dbpath ./shard02/shard02c --port 47029 --fork --shardsvr
 
-# Start config server and mongos
+# Start config server
 mongod --logpath "cfg.log" --dbpath ./cfg/ --port 57040 --fork --configsvr
+
+# Now, start mongos as well
 mongos --logpath "mongos.log" --configdb localhost:57040 --fork
 
-
-# Configure rs
+# Configure shard01
 mongo --port 47017 << 'EOF'
 rs.initiate(
-    { _id: "s1", members:[
+    { _id: "shard01", members:[
         { _id : 0, host : "localhost:47017" },
         { _id : 1, host : "localhost:47018" },
         { _id : 2, host : "localhost:47019" }]
+    });
+EOF
+
+# Configure shard02
+mongo --port 47017 << 'EOF'
+rs.initiate(
+    { _id: "shard02", members:[
+        { _id : 0, host : "localhost:47027" },
+        { _id : 1, host : "localhost:47028" },
+        { _id : 2, host : "localhost:47029" }]
     });
 EOF
 
